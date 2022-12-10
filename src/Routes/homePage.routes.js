625261c107f782bb593cdb74b0af64a73e9ff763
unknown <Amiram.ab@gmail.com>
2022-12-10 20:02:24 +0200

    v1

--- src/Routes/homePage.routes.js
+++ src/Routes/homePage.routes.js
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 
 export const homePageRouter = Router();
-export const instruction = () => {
-    return 'hey';
+
+export const instruction = (req, res) => {
+    return res.status(200).json({
+        status: 'success',
+        message: `{get all users: https://weekend14-project-nodejs-bank-api.onrender.com/users 
+        get user: https://weekend14-project-nodejs-bank-api.onrender.com/users/10 }`,
+    });
 };
 
-homePageRouter.route(``).get(instruction);
+homePageRouter.route(`/`).get(instruction);
